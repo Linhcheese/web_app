bc06b6a83734cf2724ec2f02e10933ebf976b064
Linhcheese <135191631+Linhcheese@users.noreply.github.com>
2025-03-31 08:52:54 +0700

    Update

--- resources/views/pages/idea.blade.php
+++ resources/views/pages/idea.blade.php
@@ -17,8 +17,17 @@
         </a>
         <span class="add-expenses-title">Khuyến nghị chi tiêu</span>
         <span></span>
-    </div>    <form action="{{route('idea_plan')}}" method="get" class="p-3">
+    </div>    
+        <form action="{{route('money_plan')}}" method="post" class="p-3">
         @csrf
+        <div class="add-expenses">
+            <!--Input expenses-->
+            <div class="add-expenses-sub">
+                <label for="charge">Số tiền thu nhập</label>
+                <input type="text" id="charge" class="input-expenses" placeholder="Nhập số tiền">
+                <input type="hidden" name="charge" id="charge-hidden">
+            </div>
+        </div>
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -28,28 +37,10 @@
                 </ul>
             </div>
         @endif
-        <div class="add-expenses">
-            <!--Input expenses-->
-            <div class="add-expenses-sub">
-                <label for="charge">Số tiền thu nhập</label>
-                <input type="text" id="charge" class="input-expenses" placeholder="Nhập số tiền">
-                <input type="hidden" name="charge" id="charge-hidden">
-            </div>
-
-            <div class="add-expenses-sub">
-                <label for="type">Cách chi tiêu</label>
-                <select name="type">
-                    <option value="1">Tận hưởng cuộc sống</option>
-                    <option value="2">Tối ưu tài chính</option>
-                    <option value="3">Tận hưởng và tối ưu tài chính</option>
-                </select>
-            </div>
-
-        </div>
     
         <!-- Submit Button -->
         <div class="add-expenses-sub text-center mt-4">
-            <button type="submit" class="button-add-expenses">Tạo kế hoạch chi tiêu</button>
+            <button type="submit" class="button-add-expenses">Đánh giá mức độ chi tiêu của bạn nhé!</button>
         </div>
     </form>
 </div>
