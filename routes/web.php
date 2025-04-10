615e4f1768735ba0adc29fe383f1bdd04155ced6
Linhcheese <135191631+Linhcheese@users.noreply.github.com>
2025-04-10 11:33:14 +0700

    Update check alert

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/idea', [HomeController::class, 'idea'])->name('idea');
     Route::post('/idea-plan', [HomeController::class, 'ideaPlan'])->name('idea_plan');
     Route::post('/money-plan', [HomeController::class, 'moneyPlan'])->name('money_plan');
+    
     Route::prefix('/master-category')->group(function() {
         Route::prefix('/income')->group(function() {
             Route::get('/', [MasterIncomeController::class, 'index'])->name('mincome.index');
